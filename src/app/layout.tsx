099d642510d9473d7abed40a5f7ee068169c3306
oscaralexander.taures <oscaralexander.taures@salud.gob.sv>
2024-04-26 15:27:24 -0600

    setting default font Monserrat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/app/components/Header/Header";
+import '@fontsource/montserrat'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-
-      </head>
       <body className={inter.className}>
         <div className="h-screen bg-white">
           <Header/>
