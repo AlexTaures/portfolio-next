b29bc0d2cd46895d06bdf7f017572c2e38a53298
oscaralexander.taures <oscaralexander.taures@salud.gob.sv>
2024-06-13 10:30:57 -0600

    fixing

--- src/app/projects/projects/Projects.tsx
+++ src/app/projects/projects/Projects.tsx
@@ -11,11 +11,13 @@ export default function Projects() {
         return (
             <div key={key} className='p-2 m-2 border-gray-500 border-dotted border-2 rounded-xl hover:scale-105 hover:shadow-2xl ease-in duration-100'>
                 <p className='bg-orange-enphasis w-4/5 m-auto text-center font-bold py-1 mb-1'>{ params.title }</p>
-                <a href={params.url} className='project-image relative'>
-                    <img src={params.image} alt="project-image"/>
-                    <p className="h-full w-full absolute top-0 left-0 bg-transparent text-transparent flex justify-center items-center text-[2em] blur-sm hover:backdrop-blur-sm hover:bg-black/40 hover:text-white hover:blur-none ease-out duration-500">
-                        Click to open
-                    </p>
+                <a href={params.url} className='project-image'>
+                    <div className='relative overflow-hidden flex items-center justify-center'>
+                        <p className="text-transparent bg-transparent absolute bottom-0 left-0 w-full h-full flex items-center justify-center text-[2em] blur-sm hover:backdrop-blur-sm hover:bg-black/40 hover:text-white hover:blur-none ease-in duration-500">
+                            Click to open
+                        </p>
+                        <img src={params.image} alt="project-image"/>
+                    </div>
                 </a>
                 <div className='text-center'>
                     <p className='text-white font-bold w-1/3 p-1 mt-2 mx-auto rounded-md bg-gray-enphasis'><strong>Summary</strong></p>
